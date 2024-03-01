74862e962ec7247cfc9f95914933910d68d90db1
Andrew Guschin <guschin@altlinux.org>
2024-03-01 15:28:45 +0400

    feat: template classes for subtask 1 and 2

---
+++ src/main/java/ru/sgu/Calc.java
@@ -0,0 +1,9 @@
+package ru.sgu;
+
+public class Calc
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Task2");
+    }
+}
---
+++ src/main/java/ru/sgu/Input.java
@@ -0,0 +1,9 @@
+package ru.sgu;
+
+public class Input
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Task3");
+    }
+}
